3f56fa2ba5505a85dee08db04240143b52efa11d
Nguyen Hieu Trung <trungnguyen.syncproject@gmail.com>
2019-01-19 17:55:31 +0700

    First day - Commit CKFinder5 integration
    Trung nek

---
+++ MyCoreAdmin/Common/MailBox.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Common
+{
+    public class MailBox
+    {
+        public void SendMail(string toEmail, string subject, string content)
+        {
+            var fromEmailAddress = "trungsendmailnek@gmail.com";
+            var fromEmailDisplayName = "Trung Mailer";
+            var fromEmailPassword = "trungbonmat";
+            var smtpHost = "smtp.gmail.com";
+            var smtpPort = "587";
+
+            bool enabledSsl = true;
+
+            string body = System.IO.File.ReadAllText(HttpContext.Current.Server.MapPath("/Assets/admin/pages/sendmailform/authenticationform.html"));
+            StringBuilder sbBody = new StringBuilder(body);
+            sbBody.Replace("@ViewBag.AutenticationCode", content);
+            sbBody.Replace("@ViewBag.MonthYear", System.DateTime.Now.ToShortDateString());
+            MailMessage message = new MailMessage(new MailAddress(fromEmailAddress, fromEmailDisplayName), new MailAddress(toEmail));
+            message.Subject = subject;
+            message.IsBodyHtml = true;
+            message.Body = sbBody.ToString();
+
+            var client = new SmtpClient();
+            client.Credentials = new NetworkCredential(fromEmailAddress, fromEmailPassword);
+            client.Host = smtpHost;
+            client.EnableSsl = enabledSsl;
+            client.Port = !string.IsNullOrEmpty(smtpPort) ? Convert.ToInt32(smtpPort) : 0;
+            client.Send(message);
+        }
+    }
+}
---
+++ MyCoreAdmin/MyCoreAdmin/Controllers/IntegrateToolController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCoreAdmin.Controllers
+{
+    public class IntegrateToolController : Controller
+    {
+        public IActionResult CKFinder5()
+        {
+            return View();
+        }
+    }
+}
