d073037ece2fa991083af962a789cabfeeb25c06
nguyenhieutrung97 <30740080+nguyenhieutrung97@users.noreply.github.com>
2019-01-20 23:08:42 +0700

    day 2nd
    Trung commit

--- MyCoreAdmin/MyCoreAdmin/Controllers/IntegrateToolController.cs
+++ MyCoreAdmin/MyCoreAdmin/Controllers/IntegrateToolController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Common;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyCoreAdmin.Controllers
 {
     public class IntegrateToolController : Controller
     {
-        public IActionResult CKFinder5()
+        public IActionResult SummerNote()
         {
             return View();
+        }
+        
+        public JsonResult SendMail(string toEmail,string subject,string body)
+        {
+            bool result = true;
+            try
+            {
+                MailBox mb = new MailBox();
+                mb.SendMail(toEmail, subject, body);
+            }
+            catch
+            {
+                result = false;
+            }
+            return new JsonResult(result);
         }
     }
 }
