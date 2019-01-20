6f91ac77f877bd7ac1509c8e4b1e7c0be8aab5ec
nguyenhieutrung97 <30740080+nguyenhieutrung97@users.noreply.github.com>
2019-01-20 10:52:03 +0700

    2nd day
    Trung commit

--- MyCoreAdmin/Common/MailBox.cs
+++ MyCoreAdmin/Common/MailBox.cs
@@ -1,34 +1,25 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
 
 namespace Common
 {
     public class MailBox
     {
-        public void SendMail(string toEmail, string subject, string content)
+        public void SendMail(string toEmail, string subject,string body)
         {
-            var fromEmailAddress = "trungsendmailnek@gmail.com";
-            var fromEmailDisplayName = "Trung Mailer";
-            var fromEmailPassword = "trungbonmat";
-            var smtpHost = "smtp.gmail.com";
-            var smtpPort = "587";
+            SmtpClient client = new SmtpClient("smtp.gmail.com");
+            client.UseDefaultCredentials = false;
+            client.Credentials = new NetworkCredential("trungsendmailnek@gmail.com", "trungbonmat");
+            client.Port = 587;
+            client.EnableSsl = true;
 
-            bool enabledSsl = true;
-
-            string body = System.IO.File.ReadAllText(HttpContext.Current.Server.MapPath("/Assets/admin/pages/sendmailform/authenticationform.html"));
-            StringBuilder sbBody = new StringBuilder(body);
-            sbBody.Replace("@ViewBag.AutenticationCode", content);
-            sbBody.Replace("@ViewBag.MonthYear", System.DateTime.Now.ToShortDateString());
-            MailMessage message = new MailMessage(new MailAddress(fromEmailAddress, fromEmailDisplayName), new MailAddress(toEmail));
-            message.Subject = subject;
-            message.IsBodyHtml = true;
-            message.Body = sbBody.ToString();
-
-            var client = new SmtpClient();
-            client.Credentials = new NetworkCredential(fromEmailAddress, fromEmailPassword);
-            client.Host = smtpHost;
-            client.EnableSsl = enabledSsl;
-            client.Port = !string.IsNullOrEmpty(smtpPort) ? Convert.ToInt32(smtpPort) : 0;
-            client.Send(message);
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("trungsendmailnek@gmail.com");
+            mailMessage.To.Add(toEmail);
+            mailMessage.Body =body;
+            mailMessage.Subject = subject;
+            client.Send(mailMessage);
         }
     }
 }
---
+++ MyCoreAdmin/ConsoleProjectTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using System;
+
+namespace ConsoleProjectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MailBox mb = new MailBox();
+                mb.SendMail("trungnguyen.1997270710@gmail.com", "Tui test cai gui mail", "Test thanh cong");
+            }
+            catch
+            {
+                Console.WriteLine("Test that bai roi");
+            }
+            Console.ReadLine();
+        }
+    }
+}
