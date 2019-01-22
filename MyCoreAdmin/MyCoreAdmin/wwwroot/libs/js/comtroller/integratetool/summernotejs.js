78757e1d276d6bb8d968c9c12cae37d048a1aa63
nguyenhieutrung97 <30740080+nguyenhieutrung97@users.noreply.github.com>
2019-01-22 12:56:19 +0700

    day 3
    Trung commit

---
+++ MyCoreAdmin/MyCoreAdmin/wwwroot/libs/js/comtroller/integratetool/summernotejs.js
@@ -0,0 +1,21 @@
+﻿var integrateTool = {
+    sendMail: function () {
+        $('#btnSendMail').off('click').on('click', function (e) {
+            e.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: '@Url.Action("SendMail","IntegrateTool")',
+                data: { toEmail: $('#iptToEmail').val(), subject: $('#iptSubject').val(), body: $('#summernote').val() },
+                success: function (result) {
+                    if (result == true) {
+                        alert("Gui mail thanh cong :))");
+                    }
+                    else {
+                        alert("Gui mail that bai :((");
+                    }
+                }
+            });
+        });
+    }
+};
+integrateTool.sendMail();
